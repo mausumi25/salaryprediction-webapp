26fdc1107ff662e457552a1179addb88a259357a
mausumi_25 <mausumirautaray2002@gmail.com>
2026-07-13 22:25:24 +0530

    Selected required features

--- train_model.py
+++ train_model.py
@@ -3,25 +3,21 @@ import pandas as pd
 # Load dataset
 df = pd.read_csv("data/survey_results_public.csv")
 
-# Select required columns
-df = df[[
-    "Country",
-    "EdLevel",
-    "YearsCodePro",
-    "Employment",
-    "ConvertedComp"
-]]
+# Keep only required columns
+df = df[
+    [
+        "Country",
+        "EdLevel",
+        "YearsCodePro",
+        "Employment",
+        "ConvertedComp"
+    ]
+]
 
-print("Missing Values:\n")
-print(df.isnull().sum())
-# Remove rows containing missing values
-df = df.dropna()
-
-print("\nShape after removing missing values:")
-print(df.shape)
-df = df[df["Employment"] == "Employed full-time"]
-
-print("\nShape after filtering full-time employees:")
-print(df.shape)
-print("\nCleaned Dataset:")
+print("Selected Columns")
 print(df.head())
+
+print("\nShape before cleaning:", df.shape)
+
+print("\nMissing Values")
+print(df.isnull().sum())
