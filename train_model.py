e8aee07aef30be05b1138f9575f87112aca3e5d3
mausumi_25 <mausumirautaray2002@gmail.com>
2026-07-14 23:44:41 +0530

    Filtered full-time employees

--- train_model.py
+++ train_model.py
@@ -27,3 +27,10 @@ df = df.dropna()
 
 print("\nShape after removing missing values:")
 print(df.shape)
+print("\nUnique Employment Values:\n")
+print(df["Employment"].unique())
+# Keep only full-time employees
+df = df[df["Employment"] == "Employed full-time"]
+
+print("\nShape after filtering full-time employees:")
+print(df.shape)
